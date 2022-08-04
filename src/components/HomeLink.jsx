76488a334362f7f3b14520826187834fabf67da5
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-08-03 21:29:19 -0300

    :recycle: refactor: creates a new component to render home link on header

---
+++ src/components/HomeLink.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Navbar, Container } from 'react-bootstrap';
+import logotype from "../images/logotype.png";
+import PropTypes from "prop-types";
+
+export default function HomeLink({ homeLink }) {
+  const { logoAlt, professionalSkill } = homeLink;
+
+  return (
+    <Navbar.Brand>
+      <Link to="/" className="d-flex align-items-center">
+        <img
+          alt={ logoAlt }
+          src={ logotype }
+          id="header-logo"
+        />
+        <Container className="margin-left">
+          <Row className="fs-5 txt">Rodrigo Marchi Silva</Row>
+          <Row className="fs-6 justify-content-center" id="header-subtitle">{ professionalSkill }</Row>
+        </Container>
+      </Link>
+    </Navbar.Brand>
+  );
+};
+
+HomeLink.propTypes = {
+  homeLink: PropTypes.shape({
+    logoAlt: PropTypes.string,
+    professionalSkill: PropTypes.string,
+  }).isRequired,
+};
