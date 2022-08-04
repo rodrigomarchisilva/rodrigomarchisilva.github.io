df8f85dff2326ee7f9936368f5448ef04fb5c45e
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-08-03 21:17:34 -0300

    :recycle: refactor: creates a new component for collapsible buttons on header

---
+++ src/components/CollapsibleContent.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Navbar, Nav, ButtonGroup } from 'react-bootstrap';
+import ExternalLink from "./ExternalLink";
+import InternalLink from "./InternalLink";
+import CustomizationButton from "./CustomizationButton";
+import PropTypes from "prop-types";
+
+export default function CollapsibleContent(props) {
+  const { internalLinks, externalLinks, customizationButtons } = props;
+
+  return (
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        { internalLinks.map(({ path, icon, name }) => (
+          <InternalLink key={ name } internalLink={ { path, icon, name } } />
+        ))}
+        { externalLinks.map(({ path, icon, name }) => (
+          <ExternalLink key={ name } externalLink={ { path, icon, name } } />
+        ))}
+      </Nav>
+      <Nav>
+        <ButtonGroup>
+          { customizationButtons.map(({ src, alt, title, onClick }) => (
+              <CustomizationButton key={ alt } customizationButton={ { src, alt, title, onClick } } />
+          ))}
+        </ButtonGroup>
+      </Nav>
+    </Navbar.Collapse>
+  );
+};
+
+CollapsibleContent.propTypes = {
+  internalLinks: PropTypes.arrayOf(PropTypes.shape({
+    path: PropTypes.string,
+    icon: PropTypes.string,
+    name: PropTypes.string,
+  })),
+  externalLinks: PropTypes.arrayOf(PropTypes.shape({
+    path: PropTypes.string,
+    icon: PropTypes.string,
+    name: PropTypes.string,
+  })),
+  customizationButtons: PropTypes.arrayOf(PropTypes.shape({
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    title: PropTypes.string,
+    onClick: PropTypes.func,
+  })),
+}.isRequired;
