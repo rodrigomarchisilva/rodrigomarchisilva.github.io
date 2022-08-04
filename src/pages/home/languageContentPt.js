3a00755e16c74843d5fb2bf00b70479c38ceb627
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-08-03 21:39:02 -0300

    :sparkles: feature: creates a new file for portuguese language content on home

---
+++ src/pages/home/languageContentPt.js
@@ -0,0 +1,13 @@
+const mainTitle = 'Rodrigo Marchi Silva - Desenvolvedor Web Full Stack';
+
+const description = `Bem vindo ao meu site! Aqui você pode ver um pouco sobre minha trajetória e habilidades. 
+Desenvolvi em React JS, utilizando Context API para gerenciamento de estado global. Adicionei também as 
+funcionalidades de alteração de tema e idioma. Além disso, inclui o meu portfolio, com os devidos links. 
+Obrigado por visitar!`;
+
+const languageContent = {
+  mainTitle,
+  description,
+}
+
+export default languageContent;
