3344ef0d7a1d8605936562b33c83d460792db42b
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-08-03 21:42:46 -0300

    :sparkles: feature: creates a new file for dark theme content on header

---
+++ src/components/header/themeContentDark.js
@@ -0,0 +1,34 @@
+import moon from "../../images/moon.png";
+import darkBackground from "../../images/dark-background.jpg";
+
+const themeButtonEn = {
+  src: moon,
+  alt: 'Moon',
+  title: 'Alternate theme',
+};
+
+const themeButtonBr = {
+  src: moon,
+  alt: 'Lua',
+  title: 'Mudar tema',
+};
+
+const ironicPhraseEn = 'Research shows that dark theme users have a bad taste...';
+const ironicPhraseBr = 'Pesquisas comprovam que usuários de tema escuro possuem mau gosto...';
+
+const mainBackground = darkBackground;
+
+const themeContentDark = {
+  en: {
+    themeButton: themeButtonEn,
+    ironicPhrase: ironicPhraseEn,
+    mainBackground,
+  },
+  pt: {
+    themeButton: themeButtonBr,
+    ironicPhrase: ironicPhraseBr,
+    mainBackground,
+  },
+};
+
+export default themeContentDark;
