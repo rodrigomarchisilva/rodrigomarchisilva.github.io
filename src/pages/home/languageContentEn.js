826d01e00e7cf5533825c58a60179d16e8b7b496
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-08-03 21:39:25 -0300

    :sparkles: feature: creates a new file for english language content on home

---
+++ src/pages/home/languageContentEn.js
@@ -0,0 +1,13 @@
+const mainTitle = 'Rodrigo Marchi Silva - Full Stack Web Developer';
+
+const description = `Welcome to my website! Here you can see a little bit about my career and skills. 
+I developed it in React JS, using Context API for global state management. I also added the possibility 
+to alternate theme and language. Besides, I included my portfolio, with the appropriate links. 
+Thank you for passing by!`;
+
+const languageContent = {
+  mainTitle,
+  description,
+}
+
+export default languageContent;
