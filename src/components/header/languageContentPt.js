28ac33ab5e6b668c606411eed479f0db3e242743
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-08-03 21:40:20 -0300

    :sparkles: feature: creates a new file for portuguese language content on header

---
+++ src/components/header/languageContentPt.js
@@ -0,0 +1,55 @@
+import flagBR from "../../images/flag-br.png";
+import floppyDisk from "../../images/floppy-disk.png";
+
+const languageButton = {
+  src: flagBR,
+  alt: "Bandeira do Brasil",
+  title: "Alterar idioma / Alternate language",
+};
+
+const saveButton = {
+  src: floppyDisk,
+  alt: "Disquete",
+  title: "Salvar preferências",
+};
+
+const internalLinks = [
+  {
+    path: 'about',
+    icon: 'file-earmark-person-fill',
+    name: 'Sobre',
+  },
+  {
+    path: 'tools',
+    icon: 'tools',
+    name: 'Ferramentas',
+  },
+  {
+    path: 'portfolio',
+    icon: 'folder-fill',
+    name: 'Portfólio',
+  }
+];
+
+const externalLinks = [
+  {
+    path: "https://www.linkedin.com/in/rodrigo-marchi-silva/",
+    icon: "linkedin",
+    name: "LinkedIn"
+  },
+];
+
+const logoAlt = 'Logotipo do Rodrigo';
+
+const professionalSkill = 'Desenvolvedor Web';
+
+const languageContent = {
+  languageButton,
+  saveButton,
+  internalLinks,
+  externalLinks,
+  logoAlt,
+  professionalSkill,
+};
+
+export default languageContent;
