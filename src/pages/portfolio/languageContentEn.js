af7dee6df0e2cf4c9f8fb51f66f54ec0b2166c82
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-08-07 19:54:19 -0300

    :sparkles: feat: adds a new project to the portfolio page

--- src/pages/portfolio/languageContentEn.js
+++ src/pages/portfolio/languageContentEn.js
@@ -1,4 +1,5 @@
 import personalWebsite from '../../images/personal-website.png';
+import lessonsLearned from '../../images/lessons-learned.png';
 
 const mainTitle = 'Portfolio';
 
@@ -6,12 +7,25 @@ const projects = [
   {
     name: 'Rodrigo Marchi Silva',
     image: personalWebsite,
-    description: 'My personal website. It is a simple website that I developed with React to show my background, skills and projects.',
+    description: `It's this website you are navigating on now! My personal website. It is a simple website that I 
+    developed with React to show my background, skills and projects.`,
     technologiesTitle: 'Technologies',
     technologies: ['React', 'Context API', 'Bootstrap'],
     appLink: 'https://rodrigomarchisilva.github.io/',
     appLinkTitle: 'View app',
     codeLink: 'https://github.com/rodrigomarchisilva/rodrigomarchisilva.github.io',
+    codeLinkTitle: 'Access source code',
+  },
+  {
+    name: 'Lessons Learned',
+    image: lessonsLearned,
+    description: `The first website I ever developed. It is completely basic, consisting of only one page, HTML + CSS. 
+    I used Bootstrap to make it responsible, but I kept the style as it was on the original, only replacing the old photo.`,
+    technologiesTitle: 'Technologies',
+    technologies: ['HTML', 'CSS', 'Bootstrap'],
+    appLink: 'https://rodrigomarchisilva.github.io/trybe-lessons-learned/',
+    appLinkTitle: 'View app',
+    codeLink: 'https://github.com/rodrigomarchisilva/trybe-lessons-learned',
     codeLinkTitle: 'Access source code',
   },
 ];
--- src/pages/portfolio/languageContentPt.js
+++ src/pages/portfolio/languageContentPt.js
@@ -1,4 +1,5 @@
 import personalWebsite from '../../images/personal-website.png';
+import lessonsLearned from '../../images/lessons-learned.png';
 
 const mainTitle = 'Portfólio';
 
@@ -6,12 +7,25 @@ const projects = [
   {
     name: 'Rodrigo Marchi Silva',
     image: personalWebsite,
-    description: 'Meu site pessoal. É um site simples que eu desenvolvi com React para contar sobre mim e mostrar habilidades e projetos.',
+    description: `É esse site em que você está agora! Meu site pessoal. É um site simples que eu desenvolvi com 
+    React para contar sobre mim e mostrar habilidades e projetos.`,
     technologiesTitle: 'Tecnologias',
     technologies: ['React', 'Context API', 'Bootstrap'],
     appLink: 'https://rodrigomarchisilva.github.io/',
     appLinkTitle: 'Ver aplicativo',
     codeLink: 'https://github.com/rodrigomarchisilva/rodrigomarchisilva.github.io',
+    codeLinkTitle: 'Acessar código fonte',
+  },
+  {
+    name: 'Lições Aprendidas',
+    image: lessonsLearned,
+    description: `O primeiro site que eu desenvolvi. É completamente básico, consistindo apenas de uma página, HTML + CSS.
+    Usei Bootstrap para torná-lo responsivo, mas mantive o estilo como era no original, apenas substituindo a foto.`,
+    technologiesTitle: 'Tecnologias',
+    technologies: ['HTML', 'CSS', 'Bootstrap'],
+    appLink: 'https://rodrigomarchisilva.github.io/trybe-lessons-learned/',
+    appLinkTitle: 'Ver aplicativo',
+    codeLink: 'https://github.com/rodrigomarchisilva/trybe-lessons-learned',
     codeLinkTitle: 'Acessar código fonte',
   },
 ];
