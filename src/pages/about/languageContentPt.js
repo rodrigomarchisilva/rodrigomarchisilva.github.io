3f0d2d71acf4929dfc97dd92447e42dbf46e7586
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-08-21 17:48:59 -0300

    :recycle: refactor: change  description from whoAmI section on page about

--- src/pages/about/languageContentPt.js
+++ src/pages/about/languageContentPt.js
@@ -9,11 +9,13 @@ const whoAmI = {
     {
       title: 'Olá!',
       subtitle: `Sou brasileiro, tenho ${setAge()} anos, moro em Jundiaí, São Paulo e estou em transição de carreira. 
-      Estudo Desenvolvimento Web Full Stack baseado em JavaScript na Trybe.`,
-      description: `Sou apaixonado por aprender e flerto com problemas de lógica. Minha principal métrica é saber se fiz meu máximo. 
-      Nas horas vagas, busco conhecimentos que agreguem na carreira e tempo de qualidade com pessoas importantes. Diariamente estudo idiomas novos, pratico musculação e esportes. 
-      Gosto de música, desenho, cinema e jogos eletrônicos. Passei por diversas carreiras, como metalurgia, logística e biomedicina, 
-      entretanto, retomei minha conexão com a tecnologia ao descobrir o universo da programação e embarcar na Trybe!`,
+      Finalizei o curso de Desenvolvimento Web Full Stack JavaScript na Trybe e tenho buscado uma vaga na área.`,
+      description: `Gosto de aprender novas coisas, e ao longo da minha carreira, tentei seguir um caminho em que eu estivesse sempre 
+      fazendo isso. Junto a minha paixão por desafios e tecnologia, e experiência com Excel, isso me levou a buscar a programação. 
+      Meu objetivo é me superar diariamente. Nas horas vagas, pratico musculação e esportes, como futebol, jiu-jitsu e boxe. 
+      Também me interesso por jogos, música (guitarrista/cantor) e desenho. Passei pela metalurgia, logística (onde trabalhei com Excel), 
+      aviação e biomedicina, mas me contrei novamente com o desenvolvimento de software. Através de Trybe e por conta própria, aprendi 
+      muito sobre JavaScript, e desejo continuar aprendendo mais. Mas... tenho a disposição e vontade para aprender novas coisas se necessário!`,
     },
   ],
 };
