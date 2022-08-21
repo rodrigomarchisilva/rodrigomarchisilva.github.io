d61719b0ced53b65af9a2e16a2d2b0555d2643d3
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-08-21 17:21:24 -0300

    :recycle: refactor: change mainTitle and description from home page

--- src/pages/home/languageContentEn.js
+++ src/pages/home/languageContentEn.js
@@ -1,8 +1,8 @@
-const mainTitle = 'Rodrigo Marchi Silva - Full Stack Web Developer';
+const mainTitle = 'Rodrigo Marchi Silva Full Stack Web Developer';
 
 const description = `Welcome to my website! Here you can see a little bit about my career and skills. 
-I developed it in React JS, using Context API for global state management. I also added the possibility 
-to alternate theme and language. Besides, I included my portfolio, with the appropriate links. 
+I added the possibility to alternate theme and language. Besides, I included my portfolio, with the appropriate links. 
+I've been refactoring the projects I did in Trybe on a daily basis, so I can add them to the portfolio as well. 
 Thank you for passing by!`;
 
 const languageContent = {
--- src/pages/home/languageContentPt.js
+++ src/pages/home/languageContentPt.js
@@ -1,9 +1,9 @@
-const mainTitle = 'Rodrigo Marchi Silva - Desenvolvedor Web Full Stack';
+const mainTitle = 'Rodrigo Marchi Silva Desenvolvedor Web Full Stack';
 
 const description = `Bem vindo ao meu site! Aqui você pode ver um pouco sobre minha trajetória e habilidades. 
-Desenvolvi em React JS, utilizando Context API para gerenciamento de estado global. Adicionei também as 
-funcionalidades de alteração de tema e idioma. Além disso, inclui o meu portfolio, com os devidos links. 
-Obrigado por visitar!`;
+Adicionei as funcionalidades de alteração de tema e idioma. Além disso, inclui o meu portfólio, com os devidos links. 
+Diariamente venho refatorando os projetos que fiz na Trybe, para poder adicioná-los no portfólio também. 
+Obrigado pela visita!`;
 
 const languageContent = {
   mainTitle,
