d311ee62b9585d6cdada46c41f903b89688d6708
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-11-08 00:05:15 -0300

    :art: style(code): transfer padding from button to its inner link

--- src/components/NavigationButton.jsx
+++ src/components/NavigationButton.jsx
@@ -7,8 +7,8 @@ export default function NavigationButton({ navigationButton }) {
   const { id, sectionTitle } = navigationButton;
 
   return (
-    <Button key={ id } className="bg-blue border radius">
-      <Link to={ `#${id}` } className="txt-shadow text-white">{ sectionTitle }</Link>
+    <Button key={ id } className="bg-blue border radius p-0">
+      <Link to={ `#${id}` } className="txt-shadow text-white py-1 px-3">{ sectionTitle }</Link>
     </Button>
   );
 };
