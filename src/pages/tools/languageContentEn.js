35ece8f1f96afad5944e1adb21fcedffad6036da
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-11-08 00:03:45 -0300

    :memo: chore(text): add object sections with section titles in both languages

--- src/pages/tools/languageContentEn.js
+++ src/pages/tools/languageContentEn.js
@@ -1,7 +1,21 @@
 const mainTitle = 'Tools';
 
+const sections = [
+  { sectionKey: 'workEnvironment', sectionTitle: 'Work environment' },
+  { sectionKey: 'base', sectionTitle: 'Base' },
+  { sectionKey: 'frontend', sectionTitle: 'Frontend' },
+  { sectionKey: 'database', sectionTitle: 'Database' },
+  { sectionKey: 'backend', sectionTitle: 'Backend' },
+  { sectionKey: 'containers', sectionTitle: 'Containers' },
+  { sectionKey: 'deployment', sectionTitle: 'Deployment' },
+  { sectionKey: 'tests', sectionTitle: 'Tests' },
+  { sectionKey: 'communication', sectionTitle: 'Communication' },
+  { sectionKey: 'agile', sectionTitle: 'Agile' },
+];
+
 const languageContent = {
-  mainTitle
+  mainTitle,
+  sections,
 };
 
 export default languageContent;
--- src/pages/tools/languageContentPt.js
+++ src/pages/tools/languageContentPt.js
@@ -1,7 +1,21 @@
 const mainTitle = 'Ferramentas';
 
+const sections = [
+  { sectionKey: 'workEnvironment', sectionTitle: 'Ambiente de trabalho' },
+  { sectionKey: 'base', sectionTitle: 'Base' },
+  { sectionKey: 'frontend', sectionTitle: 'Frontend' },
+  { sectionKey: 'database', sectionTitle: 'Banco de dados' },
+  { sectionKey: 'backend', sectionTitle: 'Backend' },
+  { sectionKey: 'containers', sectionTitle: 'Containers' },
+  { sectionKey: 'deployment', sectionTitle: 'Deployment' },
+  { sectionKey: 'tests', sectionTitle: 'Testes' },
+  { sectionKey: 'communication', sectionTitle: 'Comunicação' },
+  { sectionKey: 'agile', sectionTitle: 'Agile' },
+];
+
 const languageContent = {
-  mainTitle
+  mainTitle,
+  sections,
 };
 
 export default languageContent;
